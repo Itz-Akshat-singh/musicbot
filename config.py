31cab6cddc20b43defbdf77afb27e45847f32aff
Itz-Akshat-singh <akshatyt068@gmail.com>
2026-01-29 00:03:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-API_ID = 33856462        # apna API ID
-API_HASH = "a2732b001a35e4a6601367e7b9599863"
-BOT_TOKEN = "8520861796:AAFnMBBzn7d4CWSKEXSqfLNU7xgrRYD-2DM"
+import os
 
-# Credit name
+API_ID = int(os.getenv("33856462"))
+API_HASH = os.getenv("a2732b001a35e4a6601367e7b9599863")
+BOT_TOKEN = os.getenv("8520861796:AAFnMBBzn7d4CWSKEXSqfLNU7xgrRYD-2DM")
+
 OWNER_NAME = "𝐘ᴀsʜ ꭙ 𝐀ᴋsʜᴀᴛ"
